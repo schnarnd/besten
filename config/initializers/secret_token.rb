9ab54170835bf206d36bd9f1baee8cbb74134cd7
schnarnd <schnarnd@netcologne.de>
2013-09-08 17:39:44 +0200

    Eintrag

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BSS::Application.config.secret_token = '4e8542b6b8148618ee3efc1c3dfb544fabfe698ce9d936b1b4b8cf31ba869d27c3680a3a3a350ee4226b045a6846cb8e0b13cd1a0a48a5b41b41bf23bcf78f8b'
